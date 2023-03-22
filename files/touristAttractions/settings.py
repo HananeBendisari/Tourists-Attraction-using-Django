36718e359feb0c5f1c9dc7e9b09193737561c654
Hanane Bendisari <77987116+HananeBendisari@users.noreply.github.com>
2023-03-22 00:51:30 -0400

    Update settings.py
    checked if secret key was encrypted after I downloaded Hebrew encryption app. Did not work as expected. deleted secret. need to dig more into this issue for next publications

--- files/touristAttractions/settings.py
+++ files/touristAttractions/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o$u8u)kiwa5$2e3slv!tpv!@mbdse#e2o78tfuafk92y3vjnc^'
+SECRET_KEY = read commit
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
